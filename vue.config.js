8401129909c248a1aea82480762eb0334f2c3756
qiperman <matveevvitya7@yandex.ru>
2020-06-26 13:04:28 +0300

    Fix typo in publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://qiperman.github.io/crypto-exchange//'
+    ? 'https://qiperman.github.io/crypto-exchange/'
     : '/',
   chainWebpack: config => {
     config.module
